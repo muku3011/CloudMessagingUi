cc00c0b557c1855f378552f8ca8c1d89085bf4e2
Mukesh Joshi <mukesh.bciit@gmail.com>
2019-04-19 20:55:34 +0200

    Basic structure in place

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MessageComponent } from './message/message.component';
+import { UsersComponent } from './users/users.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: MessageComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UsersComponent } from './users/users.component';
+import { MessageComponent } from './message/message.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    UsersComponent,
+    MessageComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
